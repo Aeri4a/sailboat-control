e7c22cf7918707f1ec9faeb664c63c9af52c0172
Aeri4a <80831299+Aeri4a@users.noreply.github.com>
2023-11-15 16:28:39 +0100

    boat anim small refactor

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,12 +5,20 @@ import BoatSimulation from './components/BoatSimulationView'
 
 const App: FC = () => {
 
+  const data = { time: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]};
+
+  const startAnimation = () => {
+
+  };
+
   return (
     <div className={styles.appContainer}>
-      <div className={styles.leftTile}></div>
+      <div className={styles.leftTile}>
+      <button onClick={startAnimation}>Start animation</button>
+      </div>
       <div className={styles.rightTile}>
         <div className={styles.timeLineTile}></div>
-        <BoatSimulation/>
+        <BoatSimulation />
       </div>
     </div>
   )
--- app/src/components/BoatSimulationView.tsx
+++ app/src/components/BoatSimulationView.tsx
@@ -6,80 +6,79 @@ import style from './BoatSimulationView.module.scss'
 
 const BoatSimulation: FC = () => {
 
-    const canvasRef = useRef(null)
+    const canvasRef = useRef(null);
 
-    const fix_dpi = (canvas : HTMLCanvasElement) => {
-        const style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0,-2)
-        const style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0,-2)
-        canvas.setAttribute("height",String(style_height*devicePixelRatio))
-        canvas.setAttribute("width",String(style_width*devicePixelRatio))
-    }
+    const fix_dpi = (canvas: HTMLCanvasElement) => {
+        const style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+        const style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+        canvas.setAttribute("height",String(style_height * devicePixelRatio));
+        canvas.setAttribute("width",String(style_width * devicePixelRatio));
+    };
 
     const draw_boat = (ctx : CanvasRenderingContext2D) => {
-        const w : number = ctx.canvas.width
-        const h : number = ctx.canvas.height
-        ctx.translate(w/2,h/2)
+        const w : number = ctx.canvas.width;
+        const h : number = ctx.canvas.height;
+        ctx.translate(w / 2, h / 2);
         
-        ctx.fillStyle = "#432307"
-        ctx.beginPath()
-        ctx.moveTo(-0.05*h,0.1*h)
-        ctx.lineTo(0.05*h,0.1*h)
-        ctx.lineTo(0.05*h,-0.1*h)
-        ctx.lineTo(0,-0.2*h)
-        ctx.lineTo(-0.05*h,-0.1*h)
-        ctx.lineTo(-0.05*h,0.1*h)
-        ctx.closePath()
-        ctx.fill()
+        ctx.fillStyle = "#432307";
+        ctx.beginPath();
+        ctx.moveTo(-0.05 * h, 0.1 * h);
+        ctx.lineTo(0.05 * h, 0.1 * h);
+        ctx.lineTo(0.05 * h, -0.1 * h);
+        ctx.lineTo(0,-0.2 * h);
+        ctx.lineTo(-0.05 * h, -0.1 * h);
+        ctx.lineTo(-0.05 * h, 0.1 * h);
+        ctx.closePath();
+        ctx.fill();
 
-        ctx.fillStyle = "#000000"
-        ctx.beginPath()
-        ctx.moveTo(0,0)
-        ctx.arc(0,0,0.008*h,0,2*Math.PI)
-        ctx.fill()
-    }
+        ctx.fillStyle = "#000000";
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.arc(0, 0, 0.008 * h,0,2 * Math.PI);
+        ctx.fill();
+    };
 
     const draw_frame = (ctx : CanvasRenderingContext2D, scale : number) => {
         ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
-        ctx.translate(ctx.canvas.width/2, ctx.canvas.height/2)
+        ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2)
         ctx.scale(scale, scale)
-        ctx.translate(-ctx.canvas.width/2, -ctx.canvas.height/2)
+        ctx.translate(-ctx.canvas.width / 2, -ctx.canvas.height / 2)
         draw_boat(ctx)
-    }
+    };
     
-    let animationFrameId: number
 
     useEffect(() => {
-        const canvas : HTMLCanvasElement = canvasRef.current!
-        const ctx : CanvasRenderingContext2D = canvas!.getContext('2d')!
+        let scale: number = 1
+        let animationFrameId: number
+        const canvas: HTMLCanvasElement = canvasRef?.current;
+        const ctx: CanvasRenderingContext2D = canvas?.getContext('2d');
 
-        var scale : number = 1
 
-        canvas.onwheel = (event : WheelEvent) => {
+        canvas.onwheel = (event: WheelEvent) => {
             scale += -event.deltaY * 0.001
-            scale = Math.max(0.125,Math.min(scale,4))
+            scale = Math.max(0.125, Math.min(scale, 4))
         }
-
+        
         const render = () => {
-            fix_dpi(canvas)
-            draw_frame(ctx, scale)
-            animationFrameId = window.requestAnimationFrame(render)
-        }
-        render()
+            fix_dpi(canvas);
+            draw_frame(ctx, scale);
+            animationFrameId = window.requestAnimationFrame(render);
+        };
+
+        render();
 
         return () => {
             window.cancelAnimationFrame(animationFrameId)
-        }
+        };
 
-    },[draw_frame])
-
-
+    },[]);
 
     return (
         <div className={style.boatTile}>
             <canvas ref={canvasRef} className={style.boatCanvas}></canvas>
             {/* <input type='range' className={style.boatInput} min='0' max="100" step="1"></input> */}
         </div>
-    )
-}
+    );
+};
 
-export default BoatSimulation
+export default BoatSimulation;
