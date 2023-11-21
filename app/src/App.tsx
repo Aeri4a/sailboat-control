ea9b69407c7ead5316dc361b9d11135eb202953e
mm-102 <maciej.kowalski@student.put.poznan.pl>
2023-11-22 00:11:28 +0100

    change animation logic & setup interpolation

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,42 +1,24 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useState } from 'react'
 import styles from './App.module.scss'
 
 import BoatSimulation from './components/BoatSimulationView'
 
 const App: FC = () => {
-  const [test, setTest] = useState(0); // looping through data (iterator)
-  const [start, setStart] = useState(false);
+  const [timeStamp, settimeStamp] = useState(0);
+  const [animRunning, setRunning] = useState(false);
 
-  const data = { time: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]};
+  const FRAME_TIME = 100; // time between physics simulation steps [ms]
 
+  // const data = { time: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]};
+  const data = {time: [...Array(36000).keys()]};
   
   const handleStartAnimation = () => {
-    setStart(true);
+    setRunning(true);
   }
   
   const handleStopAnimation = () => {
-    setStart(false);
+    setRunning(false);
   }
-  
-  const doAnimation = () => {
-    if (test > 9)
-      setTest(0);
-    else {
-      setTest(prevState => prevState + 1);
-    }
-  };
-
-  useEffect(() => {
-    if (start) {
-      const id = setInterval(() => {
-        doAnimation();
-      }, 1000);
-
-      return () => {
-        clearInterval(id);
-      }
-    }
-  }, [test, start]);
 
   return (
     <div className={styles.appContainer}>
@@ -48,14 +30,14 @@ const App: FC = () => {
           <div className={styles.variables}>
             <button onClick={handleStartAnimation}>Start</button>
             <button onClick={handleStopAnimation}>Stop</button>
-            <button onClick={() => { setTest(0); }}>Reset</button>
+            <button onClick={() => { settimeStamp(0); }}>Reset</button>
           </div>
         </div>
       </div>
       <div className={styles.rightTile}>
         <div className={styles.timeLineTile}>TL</div>
         <div className={styles.animationWrapper}>
-          <BoatSimulation pos={data.time[test]} />
+          <BoatSimulation set_time={settimeStamp} anim_running={animRunning} frame_len={FRAME_TIME} time={data.time[timeStamp]} />
         </div>
       </div>
     </div>
--- app/src/components/BoatSimulationView.tsx
+++ app/src/components/BoatSimulationView.tsx
@@ -2,10 +2,13 @@ import { FC, useRef, useEffect } from 'react'
 import style from './BoatSimulationView.module.scss'
 
 interface BoatSimulationProps {
-    pos: number; // that would be more advanced object with at least 2 coordinates
+    set_time: (new_state: (prev: number) => number) => void;
+    anim_running: boolean;
+    frame_len: number;
+    time: number;
 }
 
-const BoatSimulation: FC<BoatSimulationProps> = ({ pos }) => {
+const BoatSimulation: FC<BoatSimulationProps> = ({ time, frame_len, set_time, anim_running }) => {
 
     const canvasRef = useRef(null);
 
@@ -16,10 +19,17 @@ const BoatSimulation: FC<BoatSimulationProps> = ({ pos }) => {
         canvas.setAttribute("width",String(style_width * devicePixelRatio));
     };
 
-    const draw_boat = (ctx : CanvasRenderingContext2D) => {
+    
+    const draw_boat = (ctx : CanvasRenderingContext2D, frame_dt: number) => {
         const w : number = ctx.canvas.width;
         const h : number = ctx.canvas.height;
-        ctx.translate((w / 2) + pos * 10, h / 2); // example of moving based on "pos" prop
+        ctx.translate(w / 2, h / 2);
+
+        const time_of_rot = 5000; //ms
+        const ct = (time+frame_dt)*frame_len;
+        const da = -2*Math.PI*(ct % time_of_rot) / time_of_rot;
+        ctx.rotate(da);
+        ctx.translate(0.2 * h, 0);
         
         ctx.fillStyle = "#432307";
         ctx.beginPath();
@@ -31,53 +41,61 @@ const BoatSimulation: FC<BoatSimulationProps> = ({ pos }) => {
         ctx.lineTo(-0.05 * h, 0.1 * h);
         ctx.closePath();
         ctx.fill();
-
+        
         ctx.fillStyle = "#000000";
         ctx.beginPath();
         ctx.moveTo(0, 0);
         ctx.arc(0, 0, 0.008 * h,0,2 * Math.PI);
         ctx.fill();
     };
-
-    const draw_frame = (ctx : CanvasRenderingContext2D, scale : number) => {
+    
+    const draw_frame = (ctx : CanvasRenderingContext2D, scale : number, frame_dt: number) => { // frame_dt will be used for interpolation
         ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
         ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2)
         ctx.scale(scale, scale)
         ctx.translate(-ctx.canvas.width / 2, -ctx.canvas.height / 2)
-        draw_boat(ctx)
+        draw_boat(ctx, frame_dt);
     };
+    
     
 
     useEffect(() => {
         let scale: number = 1
         let animationFrameId: number
-        const canvas: HTMLCanvasElement = canvasRef?.current;
-        const ctx: CanvasRenderingContext2D = canvas?.getContext('2d');
-
+        const canvas: HTMLCanvasElement = canvasRef!.current!;
+        const ctx: CanvasRenderingContext2D = canvas!.getContext('2d')!;
+        let last_frame_stamp: number = 0;
 
         canvas.onwheel = (event: WheelEvent) => {
             scale += -event.deltaY * 0.001
             scale = Math.max(0.125, Math.min(scale, 4))
         }
         
-        const render = () => {
+        const render = (currentTime: number) => {
+            if(!last_frame_stamp || !anim_running)
+                last_frame_stamp = currentTime;
+            let time_diff = currentTime - last_frame_stamp;
+            if(time_diff > frame_len){
+                set_time(prev => prev + 1);
+                last_frame_stamp = currentTime;
+                time_diff = frame_len;
+            }
             fix_dpi(canvas);
-            draw_frame(ctx, scale);
+            draw_frame(ctx, scale, time_diff / frame_len);
             animationFrameId = window.requestAnimationFrame(render);
         };
-
-        render();
+        animationFrameId = window.requestAnimationFrame(render);
 
         return () => {
             window.cancelAnimationFrame(animationFrameId)
         };
 
-    },[pos]);
+    },[time,anim_running]);
+
 
     return (
         <div className={style.boatTile}>
             <canvas ref={canvasRef} className={style.boatCanvas}></canvas>
-            {/* <input type='range' className={style.boatInput} min='0' max="100" step="1"></input> */}
         </div>
     );
 };
